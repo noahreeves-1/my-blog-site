204f2bd2cb286f407be31eb1d4d80a9c42d318ec
Maduka Dilshan <mdilshan21@gmail.com>
2022-12-18 21:38:28 +0530

    fix: remove unwanted import

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,19 +1,18 @@
 import { IUser } from "../models/user";
-import { Request } from "express";
 
-export interface IUserRequest extends Request {
-  user?: IUser; // or any other type
-}
+// export interface IUserRequest extends Request {
+//   user?: IUser; // or any other type
+// }
 
 // import { IUser } from "../models/user";
 
-// declare global {
-//   namespace Express {
-//     export interface Request {
-//       user?: IUser;
-//     }
-//   }
-// }
+declare global {
+  namespace Express {
+    export interface Request {
+      user?: IUser;
+    }
+  }
+}
 
 // declare namespace Express {
 //   export interface Request {
